be047d509a42723b6edebcdcdc9b54ba431109c6
simo-webwikinger <100198719+simo-webwikinger@users.noreply.github.com>
2025-10-16 12:31:20 +0200

    Update Turnstile site key in nuxt.config.ts

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -63,7 +63,7 @@ export default defineNuxtConfig({
       apiEndpoint: process.env.API_ENDPOINT,
       isDev: process.env.NODE_ENV === 'development',
       cookieGroups: cookieConfig,
-      turnstileSiteKey: process.env?.TURNSTILESITEKEY ?? '0x4AAAAAAANx3aXDh7UR35x0',
+      turnstileSiteKey: process.env?.TURNSTILESITEKEY ?? '0x4AAAAAABd2DZcPE5eIhCXZ',
       useAvif: process.env?.IMAGEAVIF === 'true',
       useWebp: process.env?.IMAGEWEBP === 'true',
       validateReturnReasons: process.env.VALIDATE_RETURN_REASONS === '1',
